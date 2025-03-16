7f962d106188c50db9fd720d55a4713b7f2b3a9f
Dhanush-M <md.dhanush.2006@gmail.com>
2025-03-16 19:11:30 +0530

    fix : bug

--- encrypt_decrypt.py
+++ encrypt_decrypt.py
@@ -4,6 +4,7 @@ from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.backends import default_backend
 from argon2.low_level import Type, hash_secret_raw
 from cryptography.exceptions import InvalidKey
+import base64
 
 ARGON2_SALT_LENGTH = 16
 IV_LENGTH = 16
@@ -14,8 +15,8 @@ def derive_key(password: str, salt: bytes) -> bytes:
     return hash_secret_raw(
         password.encode(),
         salt,
-        time_cost=3,
-        memory_cost=65536,
+        time_cost=5,
+        memory_cost=131072,
         parallelism=2,
         hash_len=KEY_LENGTH,
         type=Type.ID
@@ -23,6 +24,12 @@ def derive_key(password: str, salt: bytes) -> bytes:
 
 def encrypt_image(image_data: bytes, password: str) -> bytes:
     """Encrypts image data and returns the ciphertext with salt and IV."""
+
+    if not isinstance(password, str):
+        raise TypeError("Password must contain string")
+    if not isinstance(image_data, bytes):
+        raise TypeError("Image data must be in bytes format")
+    
     salt = os.urandom(ARGON2_SALT_LENGTH)
     iv = os.urandom(IV_LENGTH)
     key = derive_key(password, salt)
@@ -33,10 +40,13 @@ def encrypt_image(image_data: bytes, password: str) -> bytes:
     image_data = padder.update(image_data) + padder.finalize()
 
     ciphertext = encryptor.update(image_data) + encryptor.finalize()
-    return salt + iv + ciphertext
+    return base64.b64encode(salt + iv + ciphertext)
+
 
 def decrypt_image(encrypted_data: bytes, password: str) -> bytes:
     """Decrypts the ciphertext and returns the original image data."""
+
+    encrypted_data = base64.b64decode(encrypted_data)
     salt, iv, ciphertext = encrypted_data[:ARGON2_SALT_LENGTH], encrypted_data[ARGON2_SALT_LENGTH:ARGON2_SALT_LENGTH + IV_LENGTH], encrypted_data[ARGON2_SALT_LENGTH + IV_LENGTH:]
     key = derive_key(password, salt)
     
